3f9835c2b5cffdc1f4cb71bab869ad0e382b3446
epirnat <edi.pirnat@gmail.com>
2020-07-25 23:58:44 +0200

    toggle and skip functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Elements
+// Elements:
 
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
@@ -7,3 +7,21 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+// functions:
+
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
+  toggle.textContent = icon;
+}
+
+function skip() {
+ video.currentTime += parseFloat(this.dataset.skip);
+}
+
